e596790f6a7f199e50e5c4dd41f289301327f8d7
zahra-m-27 <zahramazhar2000@gmail.com>
2024-02-27 21:44:11 +0330

    add frontend url to server setting

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ require("dotenv").config();
 app.use(express.json());
 app.use(
   cors({
-    origin: "*", // allow the server to accept request from different origin
+    origin: "https://healthcare-appointment-booking-z6ma.onrender.com",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true, // allow session cookie from browser to pass through
   })
